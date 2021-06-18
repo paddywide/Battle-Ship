84b59399ed0f60bbc1b844bb8805c009fefd8cb5
wei.song <wei.song.work@gmail.com>
2021-06-18 23:25:09 +1000

    Add project files.

---
+++ Battle Ship/Entity/Board.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle_Ship.Entity
+{
+    public class Board
+    {
+
+
+    }
+}
